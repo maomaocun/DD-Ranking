8b2a6cbfc79ebfbda3500b7c841373046fdca54c
lizekai-richard <lzknus@gmail.com>
2024-12-14 18:32:40 +0800

    Add objective test

---
+++ test_obj.py
@@ -0,0 +1,13 @@
+import os
+import torch
+from dd_ranking import DD_Ranking_Objective, Soft_Label_Objective, KL_Divergence_Objective
+
+syn_images = torch.load("./datm/images_best.pt", map_location='cpu')
+syn_labels = torch.load("./datm/labels_best.pt", map_location='cpu')
+syn_lr = torch.load("./datm/lr_best.pt", map_location='cpu')
+
+convd3_sl_obj = Soft_Label_Objective(dataset="CIFAR10", real_data_path="./dataset", ipc=10, model_name="ConvNet-3")
+print(convd3_sl_obj.compute_metrics(syn_images, syn_labels, syn_lr))
+
+convd3_kl_obj = KL_Divergence_Objective(dataset="CIFAR10", real_data_path="./dataset", ipc=10, model_name="ConvNet-3")
+print(convd3_kl_obj.compute_metrics(syn_images, syn_labels, syn_lr))
