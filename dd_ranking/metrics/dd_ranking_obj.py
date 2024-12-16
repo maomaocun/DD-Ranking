9eb95d1a966cb3690ff29dcee23975eab8c25684
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2024-12-16 09:46:26 +0800

    Fix bugs

--- dd_ranking/metrics/dd_ranking_obj.py
+++ dd_ranking/metrics/dd_ranking_obj.py
@@ -6,21 +6,22 @@ import numpy as np
 import random
 from typing import List
 from torch import Tensor
+from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from dd_ranking.utils import build_model
-from dd_ranking.utils import TensorDataset, get_random_images, get_dataset
-from dd_ranking.utils import set_seed
-from dd_ranking.hard_label import compute_hard_label_metrics
-from dd_ranking.train import train_one_epoch, validate
+from dd_ranking.utils.utils import build_model
+from dd_ranking.utils.utils import TensorDataset, get_random_images, get_dataset
+from dd_ranking.utils.utils import set_seed
+from dd_ranking.utils.utils import train_one_epoch, validate
 
 
 class DD_Ranking_Objective:
 
-    def __init__(self, dataset: str, real_data_path: str, ipc: int, model_name: str, use_default_transform: bool=True, num_eval: int=5, 
+    def __init__(self, dataset: str, real_data_path: str, ipc: int, model_name: str, 
+                 use_default_transform: bool=True, num_eval: int=5, im_size: tuple=(32, 32),
                  num_epochs: int=100, lr: float=0.01, batch_size: int=256,
                  custom_transform: transforms.Compose=None, device: str="cuda"):
-        channel, im_size, num_classes, dst_train, dst_test, class_map, class_map_inv = get_dataset(dataset, real_data_path)
+        channel, im_size, num_classes, dst_train, dst_test, class_map, class_map_inv = get_dataset(dataset, real_data_path, im_size, custom_transform)
         self.images_train, self.labels_train, self.class_indices_train = self.load_real_data(dst_train, class_map, num_classes)
         self.test_loader = DataLoader(dst_test, batch_size=batch_size, shuffle=False)
 
@@ -30,6 +31,7 @@ class DD_Ranking_Objective:
         self.ipc = ipc
 
         # training info
+        self.model_name = model_name
         self.num_eval = num_eval
         self.batch_size = batch_size
         self.num_epochs = num_epochs
@@ -42,7 +44,7 @@ class DD_Ranking_Objective:
         class_indices = [[] for c in range(num_classes)]
         for i, (image, label) in enumerate(dataset):
             images_all.append(torch.unsqueeze(image, 0))
-            labels_all.append(class_map[label].item())
+            labels_all.append(class_map[label])
         images_all = torch.cat(images_all, dim=0)
         labels_all = torch.tensor(labels_all)
         for i, label in enumerate(labels_all):
@@ -55,9 +57,9 @@ class DD_Ranking_Objective:
 
 
 class Soft_Label_Objective(DD_Ranking_Objective):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.teacher_model = build_model(self.model_name, num_classes=self.num_classes, im_size=self.im_size, pretrained=True, device=self.device)
+    def __init__(self, dataset: str, real_data_path: str, ipc: int, model_name: str, device: str="cuda", *args, **kwargs):
+        super().__init__(dataset=dataset, real_data_path=real_data_path, ipc=ipc, model_name=model_name, device=device, *args, **kwargs)
+        self.teacher_model = build_model(model_name, num_classes=self.num_classes, im_size=self.im_size, pretrained=True, device=self.device)
         self.teacher_model.eval()
 
     @staticmethod
@@ -75,31 +77,29 @@ class Soft_Label_Objective(DD_Ranking_Objective):
         train_loader = DataLoader(hard_label_dataset, batch_size=self.batch_size, shuffle=True)
 
         loss_fn = torch.nn.CrossEntropyLoss()
-        optimizer = torch.optim.SGD(model.parameters(), lr=self.lr, weight_decay=1e-4, momentum=0.9)
-        # lr_scheduler = CosineAnnealingLR(optimizer, T_max=self.num_epochs * len(train_loader))
+        optimizer = torch.optim.SGD(model.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.0005)
+        lr_scheduler = CosineAnnealingLR(optimizer, T_max=self.num_epochs * len(train_loader))
 
         best_acc1 = 0
         for epoch in range(self.num_epochs):
-            train_one_epoch(model, train_loader, loss_fn, optimizer, device=self.device)
+            train_one_epoch(epoch, model, train_loader, loss_fn, optimizer, device=self.device)
             metric = validate(model, self.test_loader, device=self.device)
             if metric['top1'] > best_acc1:
                 best_acc1 = metric['top1']
 
         return best_acc1
         
-    def compute_soft_label_metrics(self, model, images, soft_labels, syn_lr=None):
-        if syn_lr is None:
-            syn_lr = self.lr
+    def compute_soft_label_metrics(self, model, images, soft_labels):
         soft_label_dataset = TensorDataset(images, soft_labels)
         train_loader = DataLoader(soft_label_dataset, batch_size=self.batch_size, shuffle=True)
 
         loss_fn = self.SoftCrossEntropy
-        optimizer = torch.optim.SGD(model.parameters(), lr=syn_lr, weight_decay=1e-4, momentum=0.9)
-        # lr_scheduler = CosineAnnealingLR(optimizer, T_max=self.num_epochs * len(train_loader))
+        optimizer = torch.optim.SGD(model.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.0005)
+        lr_scheduler = CosineAnnealingLR(optimizer, T_max=self.num_epochs * len(train_loader))
         
         best_acc1 = 0
         for epoch in range(self.num_epochs):
-            train_one_epoch(model, train_loader, loss_fn, optimizer, device=self.device)
+            train_one_epoch(epoch, model, train_loader, loss_fn, optimizer, device=self.device)
             metric = validate(model, self.test_loader, device=self.device)
             if metric['top1'] > best_acc1:
                 best_acc1 = metric['top1']
@@ -118,7 +118,9 @@ class Soft_Label_Objective(DD_Ranking_Objective):
         soft_labels = torch.cat(soft_labels, dim=0)
         return soft_labels
     
-    def compute_metrics(self, syn_images, soft_labels, syn_lr):
+    def compute_metrics(self, syn_images, soft_labels, lr=None):
+        if lr:
+            self.lr = lr
         obj_metrics = []
         for i in range(self.num_eval):
             set_seed()
@@ -136,7 +138,7 @@ class Soft_Label_Objective(DD_Ranking_Objective):
 
             print("Caculating syn data soft label metrics...")
             model = build_model(self.model_name, num_classes=self.num_classes, im_size=self.im_size, pretrained=False, device=self.device)
-            syn_data_soft_label_acc = self.compute_soft_label_metrics(model, syn_images, soft_labels, syn_lr=syn_lr)
+            syn_data_soft_label_acc = self.compute_soft_label_metrics(model, syn_images, soft_labels)
             del model
             
             print("Caculating random data soft label metrics...")
@@ -157,11 +159,13 @@ class Soft_Label_Objective(DD_Ranking_Objective):
 
 
 class KL_Divergence_Objective(DD_Ranking_Objective):
-    def __init__(self, teacher_model: str, temperature: float=1.0, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.teacher_model = teacher_model
+    def __init__(self, dataset: str, real_data_path: str, ipc: int, stu_model_name: str, 
+                 tea_model_name: str, temperature: float=1.0, *args, **kwargs):
+        super().__init__(dataset=dataset, real_data_path=real_data_path, ipc=ipc, model_name=stu_model_name, *args, **kwargs)
+        self.tea_model_name = tea_model_name
         self.temperature = temperature
-        self.teacher_model = self.build_model(self.teacher_model)
+        self.teacher_model = build_model(tea_model_name, num_classes=self.num_classes, im_size=self.im_size, 
+                                         pretrained=True, device=self.device)
 
     @staticmethod
     def KLDivLoss(stu_outputs, tea_outputs):
@@ -182,7 +186,7 @@ class KL_Divergence_Objective(DD_Ranking_Objective):
 
         best_acc1 = 0
         for epoch in range(self.num_epochs):
-            train_one_epoch(model, train_loader, loss_fn, optimizer, lr_scheduler=lr_scheduler, device=self.device)
+            train_one_epoch(epoch, model, train_loader, loss_fn, optimizer, lr_scheduler=lr_scheduler, device=self.device)
             metric = validate(model, self.test_loader, device=self.device)
             if metric['top1'] > best_acc1:
                 best_acc1 = metric['top1']
@@ -201,36 +205,38 @@ class KL_Divergence_Objective(DD_Ranking_Objective):
 
         best_acc1 = 0
         for epoch in range(self.num_epochs):
-            train_one_epoch(model, train_loader, loss_fn, optimizer, lr_scheduler=lr_scheduler, device=self.device)
+            train_one_epoch(epoch, model, train_loader, loss_fn, optimizer, lr_scheduler=lr_scheduler, device=self.device)
             metric = validate(model, self.test_loader, device=self.device)
             if metric['top1'] > best_acc1:
                 best_acc1 = metric['top1']
         
         return best_acc1
     
-    def compute_metrics(self, images, labels=None):
+    def compute_metrics(self, images, labels=None, lr=None):
+        if lr:
+            self.lr = lr
         obj_metrics = []
         for i in range(self.num_eval):
             set_seed()
             print(f"{i+1}th Evaluation")
 
             print("Caculating syn data hard label metrics...")
-            model = self.build_model(self.model_name)
+            model = build_model(self.model_name, num_classes=self.num_classes, im_size=self.im_size, pretrained=False, device=self.device)
             syn_data_hard_label_acc = self.compute_hard_label_metrics(model, images, hard_labels=None)
             del model
 
             print("Caculating full data hard label metrics...")
-            model = self.build_model(self.model_name)
+            model = build_model(self.model_name, num_classes=self.num_classes, im_size=self.im_size, pretrained=False, device=self.device)
             full_data_hard_label_acc = self.compute_hard_label_metrics(model, self.images_train, hard_labels=self.labels_train)
             del model
 
             print("Caculating syn data kl divergence metrics...")
-            model = self.build_model(self.model_name)
+            model = build_model(self.model_name, num_classes=self.num_classes, im_size=self.im_size, pretrained=False, device=self.device)
             syn_data_kl_divergence_acc = self.compute_kl_divergence_metrics(model, images, labels=labels)
             del model
 
             print("Caculating random data kl divergence metrics...")
-            model = self.build_model(self.model_name)
+            model = build_model(self.model_name, num_classes=self.num_classes, im_size=self.im_size, pretrained=False, device=self.device)
             random_images = get_random_images(self.images_train, self.class_indices_train, self.ipc)
             random_data_kl_divergence_acc = self.compute_kl_divergence_metrics(model, random_images, labels=labels)
             del model
