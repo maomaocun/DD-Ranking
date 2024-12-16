be8aafc6c2b4d9187942f82233aae27a979f0a17
lizekai-richard <lzknus@gmail.com>
2024-12-16 05:21:09 +0000

    Update test

--- test_obj.py
+++ test_obj.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import torch
 from dd_ranking import DD_Ranking_Objective, Soft_Label_Objective, KL_Divergence_Objective
@@ -17,19 +16,3 @@ print(convd3_sl_obj.compute_metrics(syn_images))
 
 convd3_kl_obj = KL_Divergence_Objective(dataset="CIFAR10", real_data_path="../DATASET", ipc=10, model_name="ConvNet-3", lr=0.01, device=device)
 print(convd3_kl_obj.compute_metrics(syn_images))
-=======
-import os
-import torch
-from dd_ranking import DD_Ranking_Objective, Soft_Label_Objective, KL_Divergence_Objective
-
-syn_images = torch.load("./datm/images_best.pt", map_location='cpu')
-syn_labels = torch.load("./datm/labels_best.pt", map_location='cpu')
-syn_lr = torch.load("./datm/lr_best.pt", map_location='cpu')
-
-device = "cuda:1"
-convd3_sl_obj = Soft_Label_Objective(dataset="CIFAR10", real_data_path="../DATASET", ipc=10, model_name="ConvNet-3", device=device)
-print(convd3_sl_obj.compute_metrics(syn_images, syn_labels, syn_lr))
-
-convd3_kl_obj = KL_Divergence_Objective(dataset="CIFAR10", real_data_path="../DATASET", ipc=10, model_name="ConvNet-3", device=device)
-print(convd3_kl_obj.compute_metrics(syn_images, syn_labels, syn_lr))
->>>>>>> ac398066538606f4e135c478bb48bca02148c89c
