063417cf1e9d537ef4e68663d8a5ca765f484b04
LIZEKAI <88566387+lizekai-richard@users.noreply.github.com>
2024-12-16 09:46:07 +0800

    Update test case

--- test_obj.py
+++ test_obj.py
@@ -6,8 +6,9 @@ syn_images = torch.load("./datm/images_best.pt", map_location='cpu')
 syn_labels = torch.load("./datm/labels_best.pt", map_location='cpu')
 syn_lr = torch.load("./datm/lr_best.pt", map_location='cpu')
 
-convd3_sl_obj = Soft_Label_Objective(dataset="CIFAR10", real_data_path="./dataset", ipc=10, model_name="ConvNet-3")
+device = "cuda:1"
+convd3_sl_obj = Soft_Label_Objective(dataset="CIFAR10", real_data_path="../DATASET", ipc=10, model_name="ConvNet-3", device=device)
 print(convd3_sl_obj.compute_metrics(syn_images, syn_labels, syn_lr))
 
-convd3_kl_obj = KL_Divergence_Objective(dataset="CIFAR10", real_data_path="./dataset", ipc=10, model_name="ConvNet-3")
+convd3_kl_obj = KL_Divergence_Objective(dataset="CIFAR10", real_data_path="../DATASET", ipc=10, model_name="ConvNet-3", device=device)
 print(convd3_kl_obj.compute_metrics(syn_images, syn_labels, syn_lr))
