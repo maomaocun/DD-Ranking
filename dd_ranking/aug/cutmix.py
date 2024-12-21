fec49d0be3434f4e02a509001440669d527203af
lizekai-richard <lzknus@gmail.com>
2024-12-21 16:05:08 +0000

    Update cutmix aug

--- dd_ranking/aug/cutmix.py
+++ dd_ranking/aug/cutmix.py
@@ -16,10 +16,28 @@ class Cutmix_Augmentation:
 
         self.cutmix_p = params["cutmix_p"]
 
+    def rand_bbox(self, size, lam):
+        W = size[2]
+        H = size[3]
+        cut_rat = np.sqrt(1.0 - lam)
+        cut_w = int(W * cut_rat)
+        cut_h = int(H * cut_rat)
+
+        # uniform
+        cx = np.random.randint(W)
+        cy = np.random.randint(H)
+
+        bbx1 = np.clip(cx - cut_w // 2, 0, W)
+        bby1 = np.clip(cy - cut_h // 2, 0, H)
+        bbx2 = np.clip(cx + cut_w // 2, 0, W)
+        bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+        return bbx1, bby1, bbx2, bby2
+
     def cutmix(self, images):
         rand_index = torch.randperm(images.size()[0]).to(images.device)
         lam = np.random.beta(self.cutmix_p, self.cutmix_p)
-        bbx1, bby1, bbx2, bby2 = rand_bbox(images.size(), lam)
+        bbx1, bby1, bbx2, bby2 = self.rand_bbox(images.size(), lam)
 
         images[:, :, bbx1:bbx2, bby1:bby2] = images[rand_index, :, bbx1:bbx2, bby1:bby2]
         return images
