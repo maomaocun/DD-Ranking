fd6bbb09cbd83529e6d9543313e77546788a21fc
lizekai-richard <lzknus@gmail.com>
2024-12-31 16:59:46 +0800

    Fix a bug

--- dd_ranking/__init__.py
+++ dd_ranking/__init__.py
@@ -1,5 +1,5 @@
 from .aug import DSA, Mixup, Cutmix, ZCAWhitening
 from .config import Config
 from .loss import KLDivergenceLoss, SoftCrossEntropyLoss
-from .metrics import HardScoreEvaluator, SoftLabelEvaluator, GeneralEvaluator
+from .metrics import HardLabelEvaluator, SoftLabelEvaluator, GeneralEvaluator
 from .utils import get_dataset, build_model, get_convnet, get_lenet, get_resnet, get_vgg, get_alexnet
